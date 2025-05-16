6007532aeb2c391d0cc85e580cf3e7e049e7090e
Kab-35-16 <Kab-35-16@KAB-31-9->
2025-05-16 12:57:04 +0300

    .

---
+++ Barrier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42_practicum
+{
+    class Barrier
+    {
+        public delegate void MethodContainer();
+        public event MethodContainer Event1;
+        public event MethodContainer Event2;
+
+        public void Method1()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Закрыто.");
+        }
+        public void Method2()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Открыто.");
+        }
+        public void Method3()
+        {
+            for (int i = 0; i < 11; i++)
+            {
+
+            }
+            Random rnd = new Random();
+            int eventChoose = rnd
+        }
+    }
+}
---
+++ Man.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _42_practicum
+{
+    class Man
+    {
+        public string name;
+
+        public delegate void MethodContainer();
+        public event MethodContainer Event1;
+        public event MethodContainer Event2;
+
+        public void Method1()
+        {
+            Console.WriteLine(name + "проходит");
+            Event1 -= Method1;
+            Event1 -= Method2;
+        }        
+        public void Method2()
+        {
+            Console.WriteLine(name + "cтоит");
+        }
+    }
+}
